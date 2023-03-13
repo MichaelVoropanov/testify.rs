e800d0bb3389821a940a8ffc3c35e622a6f0be28
michael-voropanov <michael.voropanov@gmail.com>
2023-03-13 11:35:12 +0000

    Broken Coords thing

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,48 @@
-#[allow(unused_variables)]
-#[allow(unused_assignments)]
-#[allow(unused_variables)]
-#[allow(unused_mut)]
-#[allow(unused_imports)]
-#[allow(dead_code)]
-use std::io;
 
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-    println!("{:#?}", bytes);
-    for (i, &item) in bytes.iter().enumerate() {
-        println!("{:#?}", item);
-        println!("{:#?}", i);
-        if item == b' ' {
-            return i;
+#[derive(Copy, Clone)]
+struct Coord(i32, i32);
+struct Line(Coord,Coord);
+
+impl Line {
+    fn get_angle(center:Coord, point:Coord) -> f32 {
+        #[allow(unused_variables)]
+        #[allow(unused_assignments)]
+        let mut angle:f32 = 0.0;
+        let x = point.0 - center.0;
+        let y = point.1 - center.1;
+        if x == 0 {
+            if y > 0 {
+                angle = 90.0;
+            } else {
+                angle = 270.0;
+            }
+        } else if y == 0 {
+            if x > 0 {
+                angle = 0.0;
+            } else {
+                angle = 180.0;
+            }
+        } else {
+            angle = (y as f32 / x as f32).atan() * 180.0 / std::f32::consts::PI;
+            if x < 0 {
+                angle += 180.0;
+            } else if y < 0 {
+                angle += 360.0;
+            }
         }
+        angle
     }
-
-    s.len()
 }
 
-fn main() {
-    let mut s = String::from("hello world");
-
-    let word = first_word(&s); // word will get the value 5
-    println!("{}", word);
-    s.clear(); // this empties the String, making it equal to ""
-
-    // word still has the value 5 here, but there's no more string that
-    // we could meaningfully use the value 5 with. word is now totally invalid!
-    let s = String::from("hello world");
-
-    let hello = &s[0..5];
-    let world = &s[6..11];
-    println!("{:#?}", hello);
-    println!("{:#?}", world);
-
+fn main() -> ! {
+   let center:Coord = Coord(0,0);
+   let mut point:Coord = Coord(1,10);
+   loop{
+       let angle1:f32 = Line::get_angle(center, point);
+       println!("Angle: {}", angle1);
+       let rng = rand::thread_rng();
+       point.1 += rng.gen_range(-10..10);
+       point.0 += rng.gen_range(-10..10);
 }
+}
+
